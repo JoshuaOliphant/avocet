98eae42b3b8981af232bf342c6be73c288ad595f
Joshua Oliphant <joshua.oliphant@hey.com>
2023-03-14 07:29:09 -0700

    Replaced old app.py with more streamlined version

--- avocet/app.py
+++ avocet/app.py
@@ -1,75 +1,52 @@
 from textual.app import App, ComposeResult
-from textual.reactive import reactive
-from textual.widgets import Input, Static, Header, Footer, Button, Label, ListItem, ListView
-from textual import events, log
-from textual.containers import Container 
-
-from rich.markdown import Markdown
+from textual.widgets import Header, Static, Input, ListView, ListItem, Label, Footer
+from textual.containers import Container, Vertical
+from textual import log
 
 from raindrop import Raindrop
 
+raindrop = Raindrop()
+
+def collection_to_list_items(collections: map):
+    return [ListItem(Label('{} {}'.format(key, value['count'])), id=key) for key, value in collections.items()]
+
+def raindrops_to_list_items(id: str = '30350988'):
+    raindrop = Raindrop()
+    items = raindrop.getRaindropsBy(id)
+    list_items = [ListItem(Label('{} {}'.format(item['title'], item['excerpt']), id="raindrop")) for item in items]
+    return list_items
 
 class URLInput(Static):
     def compose(self) -> ComposeResult:
         yield Input(placeholder="https://", id="input")
 
-class CollectionItem(ListItem):
-    def __init__(self, id):
-        self.id = id
-        super().__init__(id=id)
-
-class Collection(ListView):
-
-    def __init__(self, id):
-        self._id = id
-        self.raindrop = Raindrop()
-        self.items = self.raindrop.getCollections()
-        self.list_items = []
-        for key, value in self.items.items():
-            self.list_items.append(ListItem(Label('{} {}'.format(key, value['count'])), id=key))
-        super().__init__(*self.list_items)
-    
-    def get_raindrops_by(self, name: str):
-        return self.items[name]
-
-class Previews(ListView):
-
-    def __init__(self, id):
-        self._id = id
-        self.raindrop = Raindrop()
-        self.items = self.raindrop.getRaindropsBy('30350988')
-        self.list_items = [ListItem(Label('{} {}'.format(item['title'], item['excerpt']), id="raindrop")) for item in self.items]
-        super().__init__(*self.list_items)
-    
-    def update_preview(self, id: str):
-        self.clear()
-        self.items = self.raindrop.getRaindropsBy(id)
-        log(f"items: {self.items}")
-        for item in self.items:
-            self.append(ListItem(Label('{} {}'.format(item['title'], item['excerpt']))))
-        # self.list_items = [ListItem(Label('{} {}'.format(item['title'], item['excerpt']), id="raindrop")) for item in self.items]
-
-class Info(App):
+class Avocet(App):
     CSS_PATH = "avocet.css"
     
     def compose(self) -> ComposeResult:
         yield Header()
+        with Container():
+            yield URLInput()
+            with Vertical(id="raindrop_collections_vertical"):
+                self.collection_map = raindrop.getCollections()
+                self.raindrop_collections = collection_to_list_items(self.collection_map)
+                yield ListView(*self.raindrop_collections, id="raindrop_collections")
+            with Vertical(id="raindrop_previews_vertical"):
+                yield ListView(*raindrops_to_list_items(), id="raindrop_previews")
         yield Footer()
-        yield Container(
-            URLInput(),
-            Collection(id="Collection"),
-            Previews(id="Previews")
-        )
         
     def on_mount(self) -> None:
         self.query_one(Input).focus()
 
-    def on_list_view_selected(self, event):
-        collections = self.query_one(Collection)
-        collection = collections.get_raindrops_by(event.item.id)
-        previews = self.query_one(Previews)
-        previews.update_preview(collection["id"])
-
+    def on_list_view_selected(self, event: ListView.Selected):
+        if event.sender.id == "raindrop_collections":
+            raindrop_collection = self.collection_map[event.item.id]
+            raindrop_previews = self.query_one("#raindrop_previews")
+            raindrop_previews.clear()
+            new_raindrops = raindrops_to_list_items(raindrop_collection["id"])
+            for raindrop in new_raindrops:
+                raindrop_previews.append(raindrop)
+    
 if __name__ == "__main__":
-    app = Info()
+    app = Avocet()
     app.run()
