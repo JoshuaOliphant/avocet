6b3bd46c500100a6c94c5680a3355ecea59380d1
Joshua Oliphant <joshuaoliphant@Joshuas-MacBook-Pro.local>
2023-03-27 21:23:40 -0700

    Small readme update. Got collection selection fixed, if a bit ugly

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Avocet
 
 Avocet is a TUI the accesses the [Raindrop API](https://developer.raindrop.io/). It is written in Python and uses the [Textual Framework](https://textual.textualize.io).
+
+## Requirements
+- Python 3
+- Poetry
--- avocet/app.py
+++ avocet/app.py
@@ -5,13 +5,11 @@ from textual import log
 
 from raindrop import Raindrop
 
-raindrop = Raindrop()
-
 def collection_to_list_items(collections: map):
     return [ListItem(Label('{} {}'.format(key, value['count'])), id=key) for key, value in collections.items()]
 
 def raindrops_to_list_items(raindrops: map):
-    return [ListItem(Label('{} | {}'.format(value['title'], value['excerpt'])), id=key) for key, value in raindrops.items()]
+    return [ListItem(Label('{} | {}'.format(value['title'], value['excerpt'])), id=f"raindrop-{key}") for key, value in raindrops.items()]
 
 class URLInput(Static):
     def compose(self) -> ComposeResult:
@@ -20,16 +18,18 @@ class URLInput(Static):
 class Avocet(App):
     CSS_PATH = "avocet.css"
 
+    raindrop = Raindrop()
+
     def compose(self) -> ComposeResult:
         yield Header()
         with Container():
             yield URLInput()
             with Horizontal(id="raindrop_collections_horizontal"):
-                self.collection_map = raindrop.getCollections()
+                self.collection_map = self.raindrop.getCollections()
                 self.raindrop_collections = collection_to_list_items(self.collection_map)
                 yield ListView(*self.raindrop_collections, id="raindrop_collections")
             with Horizontal(id="raindrop_previews_horizontal"):
-                self.raindrop_map = raindrop.getRaindropsBy("30350988")
+                self.raindrop_map = self.raindrop.getRaindropsBy("30350988")
                 yield ListView(*raindrops_to_list_items(self.raindrop_map), id="raindrop_previews")
         yield Footer()
 
@@ -37,10 +37,13 @@ class Avocet(App):
         self.query_one(Input).focus()
 
     def handle_raindrop_collection_selected(self, event: ListView.Selected):
+        event_item = event.item
         raindrop_collection = self.collection_map[event.item.id]
+        id = raindrop_collection["id"]
         raindrop_previews = self.query_one("#raindrop_previews")
         raindrop_previews.clear()
-        new_raindrops = raindrops_to_list_items(raindrop_collection["id"])
+        new_raindrops_collection = self.raindrop.getRaindropsBy(str(id))
+        new_raindrops = raindrops_to_list_items(new_raindrops_collection)
         for raindrop in new_raindrops:
             raindrop_previews.append(raindrop)
 
