e77edbe149a5d433b737e25f5c8bdc41ff0cffe3
Joshua Oliphant <joshua.oliphant@hey.com>
2023-08-26 10:05:15 -0700

    Added new unit tests for the raindrop api

--- avocet/raindrop_api.py
+++ avocet/raindrop_api.py
@@ -8,35 +8,23 @@ class RaindropAPI:
     def __init__(self) -> None:
         token = os.environ["RAINDROP"]
         self._headers = {"Authorization": f"Bearer {token}"}
+        self._client = httpx.AsyncClient()
 
     async def get_collections(self) -> Dict:
-        async with httpx.AsyncClient() as client:
-            r = await client.get("https://api.raindrop.io/rest/v1/collections", headers=self._headers)
+        r = await self._client.get("https://api.raindrop.io/rest/v1/collections", headers=self._headers)
         return r.json()['items']
 
     async def get_raindrops_by_collection_id(self, collection_id: str, search: str = None) -> Dict:
-        async with httpx.AsyncClient() as client:
-            if search:
-                params = {"search": search}
-                r = await client.get(f"https://api.raindrop.io/rest/v1/raindrops/{collection_id}", headers=self._headers, params=params)
-            else:
-                r = await client.get(f"https://api.raindrop.io/rest/v1/raindrops/{collection_id}", headers=self._headers)
+        if search:
+            params = {"search": search}
+            r = await self._client.get(f"https://api.raindrop.io/rest/v1/raindrops/{collection_id}", headers=self._headers, params=params)
+        else:
+            r = await self._client.get(f"https://api.raindrop.io/rest/v1/raindrops/{collection_id}", headers=self._headers)
         return r.json()['items']
 
     async def get_raindrop_by_raindrop_id(self, raindrop_id: str) -> Dict:
-        async with httpx.AsyncClient() as client:
-            r = await client.get(f"https://api.raindrop.io/rest/v1/raindrop/{raindrop_id}", headers=self._headers)
-        item = r.json()['item']
-        raindrop = dict()
-        raindrop[item['_id']] = {
-            'excerpt': item['excerpt'],
-            'tags': item['tags'],
-            'title': item['title'],
-            'link': item['link'],
-            'type': 'raindrop'
-        }
-        r.close()
-        return raindrop
+        r = await self._client.get(f"https://api.raindrop.io/rest/v1/raindrop/{raindrop_id}", headers=self._headers)
+        return r.json()['item']
 
     async def post_raindrop(self, url: str, collection: str, tags: list) -> None:
         raindrop = {
@@ -45,7 +33,6 @@ class RaindropAPI:
             "tags": tags
         }
         log(f"Posting raindrop: {raindrop}")
-        async with httpx.AsyncClient() as client:
-            r = await client.post("https://api.raindrop.io/rest/v1/raindrop", headers=self._headers, json=raindrop)
+        r = await self._client.post("https://api.raindrop.io/rest/v1/raindrop", headers=self._headers, json=raindrop)
         log(f"Raindrop posted: {r.json()}")
         r.close()
---
+++ tests/test_raindrop_api.py
@@ -0,0 +1,51 @@
+import pytest
+from httpx import Response
+from pytest_httpx import HTTPXMock
+from avocet.raindrop_api import RaindropAPI
+
+@pytest.fixture
+def api():
+    return RaindropAPI()
+
+@pytest.mark.asyncio
+async def test_get_collections(api: RaindropAPI, httpx_mock: HTTPXMock):
+    # Mock the HTTP response
+    httpx_mock.add_response(url="https://api.raindrop.io/rest/v1/collections", json={"items": [{"id": "1", "title": "Collection 1"}, {"id": "2", "title": "Collection 2"}]})
+
+    # Call the method being tested
+    collections = await api.get_collections()
+
+    # Check the result
+    assert len(collections) == 2
+    assert collections[0]["id"] == "1"
+    assert collections[0]["title"] == "Collection 1"
+    assert collections[1]["id"] == "2"
+    assert collections[1]["title"] == "Collection 2"
+
+@pytest.mark.asyncio
+async def test_get_raindrops_by_collection_id(api: RaindropAPI, httpx_mock: HTTPXMock):
+    # Mock the HTTP response
+    httpx_mock.add_response(url="https://api.raindrop.io/rest/v1/raindrops/1", json={"items": [{"id": "1", "title": "Raindrop 1"}, {"id": "2", "title": "Raindrop 2"}]})
+
+    # Call the method being tested
+    raindrops = await api.get_raindrops_by_collection_id("1")
+
+    # Check the result
+    assert len(raindrops) == 2
+    assert raindrops[0]["id"] == "1"
+    assert raindrops[0]["title"] == "Raindrop 1"
+    assert raindrops[1]["id"] == "2"
+    assert raindrops[1]["title"] == "Raindrop 2"
+
+@pytest.mark.asyncio
+async def test_get_raindrop_by_raindrop_id(api: RaindropAPI, httpx_mock: HTTPXMock):
+    # Mock the HTTP response
+    httpx_mock.add_response(url="https://api.raindrop.io/rest/v1/raindrop/1", 
+                            json={"item":{"id": "1", "title": "Raindrop 1", "excerpt": "Excerpt 1", "link": "https://example.com/1", "tags": ["tag1", "tag2"]}})
+
+    # Call the method being tested
+    raindrop = await api.get_raindrop_by_raindrop_id("1")
+
+    # Check the result
+    assert raindrop["id"] == "1"
+    assert raindrop["title"] == "Raindrop 1"
