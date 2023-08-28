c37ca160b7f19d18160bae4cac55c50c1947efd0
Joshua Oliphant <joshua.oliphant@hey.com>
2023-08-28 06:45:46 -0700

    Added a progress by while loading the app for the first time. Change the css extenstion to tcss. The changes for the progress bar seem to have made the app load faster.

--- avocet/app.py
+++ avocet/app.py
@@ -1,20 +1,27 @@
+import asyncio
 import os
 import time
 import webbrowser
 from textual import on, work, log
 from textual.app import App, ComposeResult
-from textual.widgets import OptionList
+from textual.containers import Center
+from textual.widgets import OptionList, ProgressBar, Label
 from textual.widgets.option_list import Option
 from sqlalchemy import create_engine
 from database_manager import DatabaseManager
+from raindrop_api import RaindropAPI
 
 class Avocet(App):
 
-    CSS_PATH = "avocet.css"
+    CSS_PATH = "avocet.tcss"
 
     def compose(self) -> ComposeResult:
-        yield OptionList(id="collection_option_list", )
-        yield OptionList(id="raindrop_option_list")
+        with Center():
+            yield Label("Loading...")
+            yield ProgressBar(total=100)
+        with Center():
+            yield OptionList(id="collection_option_list")
+            yield OptionList(id="raindrop_option_list")
 
     async def on_mount(self) -> None:
         self.startup()
@@ -25,27 +32,36 @@ class Avocet(App):
         start_time = time.time()
         db_name = os.environ.get("DB_NAME", "avocet")
         db_path = f'{db_name}.sqlite'
+        raindrop_api = RaindropAPI()
 
         if not os.path.exists(db_path):
             self.engine = create_engine(f'sqlite:///{db_path}')
             self.database_manager = DatabaseManager(self.engine)
             self.database_manager.create_tables()
-            await self.database_manager.add_collections()
+            collection_data_list = await raindrop_api.get_collections()
+            self.query_one(ProgressBar).update(total=len(collection_data_list))
+            for collection_data in collection_data_list:
+                self.database_manager.add_collection(collection_data)
+                raindrop_data_list = await raindrop_api.get_raindrops_by_collection_id(collection_data["_id"])
+                self.database_manager.add_raindrops(raindrop_data_list, collection_data["_id"])
+                self.query_one(ProgressBar).advance(1)
         else:
             self.engine = create_engine(f'sqlite:///{db_path}')
             self.database_manager = DatabaseManager(self.engine)
-        await self.initialize_view()
+        self.query_one(ProgressBar).remove()
+        self.query_one(Label).remove()
+        self.initialize_view()
         end_time = time.time()
         log(f"Startup time: {start_time-end_time}")
 
-    async def initialize_view(self):
-        collections = await self.database_manager.get_collections()
+    def initialize_view(self):
+        collections = self.database_manager.get_collections()
         for collection in collections:
             collection_options = [Option(prompt=collection.title, id=collection.id)]
             collection_option_list = self.query_one("#collection_option_list")
             collection_option_list.add_options(collection_options)
         if collections:
-            raindrops = await self.database_manager.get_raindrops_by_collection_id(collections[0].id)
+            raindrops = self.database_manager.get_raindrops_by_collection_id(collections[0].id)
         else:
             raindrops = []
         raindrop_options = [Option(prompt=raindrop.title, id=raindrop.id) for raindrop in raindrops]
@@ -53,9 +69,9 @@ class Avocet(App):
         raindrop_option_list.add_options(raindrop_options)
 
     @on(OptionList.OptionSelected, selector="#collection_option_list")
-    async def select_collection(self, event: OptionList.OptionSelected):
+    def select_collection(self, event: OptionList.OptionSelected):
         collection_id = event.option.id
-        raindrops = await self.database_manager.get_raindrops_by_collection_id(collection_id)
+        raindrops = self.database_manager.get_raindrops_by_collection_id(collection_id)
         options = [Option(prompt=raindrop.title, id=raindrop.id) for raindrop in raindrops]
         option_list = self.query_one("#raindrop_option_list")
         option_list.clear_options()
--- avocet/database_manager.py
+++ avocet/database_manager.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from sqlalchemy.orm import sessionmaker
 from models import Collection, Base, Raindrop
-from raindrop_api import RaindropAPI
 
 class DatabaseManager:
     def __init__(self, engine):
@@ -12,24 +11,19 @@ class DatabaseManager:
     def create_tables(self):
         Base.metadata.create_all(self.engine)
 
-    async def add_collections(self):
+    def add_collection(self, collection_data):
         session = self.Session()
-        raindrop_api = RaindropAPI()
-
-        collection_data_list = await raindrop_api.get_collections()
-        for collection_data in collection_data_list:
-            collection = Collection(id=collection_data['_id'],
-                                    title=collection_data['title'],
-                                    description=collection_data["description"],
-                                    count=collection_data['count'],
-                                    created=datetime.strptime(collection_data['created'], "%Y-%m-%dT%H:%M:%S.%fZ"),
-                                    last_update=datetime.strptime(collection_data['lastUpdate'], "%Y-%m-%dT%H:%M:%S.%fZ"))
-            session.add(collection)
-            await self.add_raindrops(collection_data=collection, session=session, raindrop_api=raindrop_api)
+        collection = Collection(id=collection_data['_id'],
+                                title=collection_data['title'],
+                                description=collection_data["description"],
+                                count=collection_data['count'],
+                                created=datetime.strptime(collection_data['created'], "%Y-%m-%dT%H:%M:%S.%fZ"),
+                                last_update=datetime.strptime(collection_data['lastUpdate'], "%Y-%m-%dT%H:%M:%S.%fZ"))
+        session.add(collection)
         session.commit()
 
-    async def add_raindrops(self, collection_data, session, raindrop_api):
-        raindrop_data_list = await raindrop_api.get_raindrops_by_collection_id(collection_data.id)
+    def add_raindrops(self, raindrop_data_list, collection_id):
+        session = self.Session()
         for raindrop_data in raindrop_data_list:
             raindrop = Raindrop(id=raindrop_data['_id'],
                                 excerpt=raindrop_data['excerpt'],
@@ -38,15 +32,16 @@ class DatabaseManager:
                                 link=raindrop_data['link'],
                                 created=datetime.strptime(raindrop_data['created'], "%Y-%m-%dT%H:%M:%S.%fZ"),
                                 last_update=datetime.strptime(raindrop_data['lastUpdate'], "%Y-%m-%dT%H:%M:%S.%fZ"),
-                                collection_id=collection_data.id)
+                                collection_id=collection_id)
             session.add(raindrop)
+        session.commit()
 
-    async def get_collections(self):
+    def get_collections(self):
         session = self.Session()
         collections = session.query(Collection).all()
         return collections
 
-    async def get_raindrops_by_collection_id(self, collection_id):
+    def get_raindrops_by_collection_id(self, collection_id):
         session = self.Session()
         raindrops = session.query(Raindrop).filter(Raindrop.collection_id == collection_id).all()
         return raindrops
