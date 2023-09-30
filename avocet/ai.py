64a19548b8003b81ed899fb9808c98f895d46605
Joshua Oliphant <joshua.oliphant@hey.com>
2023-09-30 12:13:42 -0700

    On initialization, get the contexts of use url and summarize it, stored in db. Now when choose a bookmark, this summary is displayed.

--- avocet/ai.py
+++ avocet/ai.py
@@ -1,52 +1,76 @@
 import openai
 import os
 from typing import Sequence
+from langchain.chat_models import ChatOpenAI
+from langchain.schema.output_parser import OutputParserException
+from langchain.chains import create_extraction_chain
 from langchain.schema import Document
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.document_loaders import WebBaseLoader
-from langchain.indexes import VectorstoreIndexCreator
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.document_transformers import Html2TextTransformer
+from langchain.text_splitter import RecursiveCharacterTextSplitter, Language
+# from langchain.document_transformers import Html2TextTransformer
 from textual import log
+from requests.exceptions import SSLError
 
 class AI:
 
     def __init__(self) -> None:
-        openai.api_key = os.environ["OPENAI"]
+        openai.api_key = os.environ["OPENAI_API_KEY"]
         self.llm_name = "gpt-3.5-turbo"
         self.persist_directory = 'docs/chroma/'
         self.embedding = OpenAIEmbeddings()
+        self.llm = ChatOpenAI(temperature=0, model=self.llm_name)
+
+    async def extract(self, content: str, schema: dict):
+        try:
+            text_splitter = RecursiveCharacterTextSplitter().from_language(language=Language.HTML)
+            text_splits = text_splitter.split_text(content[0].page_content)
+            print(f"Length of text_splits: {len(text_splits)}")
+            if len(text_splits) == 0:
+                return None
+            return create_extraction_chain(schema=schema, llm=self.llm).run(text_splits[0])
+        except OutputParserException as e:
+            # Handle the exception here
+            print(f"Error occurred during output parsing: {e}")
+            return None
+        except SSLError as e:
+            print(f"SSL error occurred: {e}")
+            return None
 
     async def html_to_markdown(self, url) -> Sequence[Document]:
         loader = WebBaseLoader(url)
         data = loader.load()
-        log(f"Loaded html: {data}")
 
-        html2text = Html2TextTransformer()
-        transformed_data = html2text.transform_documents(data)
-        log(f"Transformed: {transformed_data}")
+        schema = {
+            "properties": {
+                "article_title": {"type": "string"},
+                "article_summary": {"type": "string"},
+            },
+            "required": ["article_title", "article_body"],
+        }
+
+        transformed_data = await self.extract(data, schema)
+        print(f"transormed data: {transformed_data}")
 
         return transformed_data
 
     async def initialize_vector_store(self, documents: Sequence[Document]):
-        # text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
-        # splits = []
-        # for document in documents:
-        #     if document:
-        #         s = text_splitter.split_text(document.page_content)
-        #         splits.extend([Document(split) for split in s])
 
         vectordb = Chroma.from_documents(
             documents=documents,
             embedding=self.embedding,
             persist_directory=self.persist_directory
         )
-        # log(f"Split length: {len(splits)}")
         log(f"Vector db collection count: f{vectordb._collection.count()}")
-        
+
         question = "Is this document about Python programming language?"
         docs = vectordb.similarity_search(question, k=3)
         log(f"Length of doc: {len(docs)}")
         log(f"First page or docs: {docs[0].page_content}")
         vectordb.persist()
+
+        assert len(docs) == 3
+        assert docs[0].page_content is not None
+        assert isinstance(docs[0].page_content, str)
+        assert vectordb._collection.count() > 0
--- avocet/app.py
+++ avocet/app.py
@@ -1,10 +1,9 @@
 import os
 import time
-import webbrowser
 from textual import on, work, log
 from textual.app import App, ComposeResult
-from textual.containers import Center
-from textual.widgets import OptionList, ProgressBar, Label
+from textual.containers import Center, VerticalScroll
+from textual.widgets import OptionList, ProgressBar, Label, MarkdownViewer
 from textual.widgets.option_list import Option
 from sqlalchemy import create_engine
 from database_manager import DatabaseManager
@@ -25,6 +24,8 @@ class Avocet(App):
         with Center():
             yield OptionList(id="collection_option_list")
             yield OptionList(id="raindrop_option_list")
+        with VerticalScroll():
+            yield MarkdownViewer(id='markdown_viewer')
 
     async def on_mount(self) -> None:
         db_name = os.environ.get("DB_NAME", "avocet")
@@ -48,7 +49,7 @@ class Avocet(App):
             await self.add_text()
         self.query_one(ProgressBar).remove()
         self.query_one(Label).remove()
-        self.initialize_view()
+        await self.initialize_view()
         end_time = time.time()
         log(f"Startup time: {start_time-end_time}")
 
@@ -62,17 +63,16 @@ class Avocet(App):
             self.database_manager.add_raindrops(raindrop_data_list, collection_data["_id"])
             self.query_one(ProgressBar).advance(1)
 
-    # @work(exclusive=True, thread=True)
     async def add_text(self):
         raindrops = self.database_manager.get_all_raindrops()
         for raindrop in raindrops:
             markdown = await self.ai.html_to_markdown(raindrop.link)
             log(f"Markdown: {markdown}")
-            raindrop.full_text = markdown[0].page_content
-            await self.ai.initialize_vector_store(markdown)
-            self.database_manager.add_raindrop(raindrop)
+            if markdown:
+                raindrop.summary = markdown[0]['article_summary']
+        self.database_manager.update_raindrops(raindrops)
 
-    def initialize_view(self):
+    async def initialize_view(self):
         collections = self.database_manager.get_collections()
         for collection in collections:
             collection_options = [Option(prompt=collection.title, id=collection.id)]
@@ -85,6 +85,8 @@ class Avocet(App):
         raindrop_options = [Option(prompt=raindrop.title, id=raindrop.id) for raindrop in raindrops]
         raindrop_option_list = self.query_one("#raindrop_option_list")
         raindrop_option_list.add_options(raindrop_options)
+        markdown_viewer = self.query_one("Markdown")
+        await markdown_viewer.update(raindrops[0].summary)
 
     @on(OptionList.OptionSelected, selector="#collection_option_list")
     def select_collection(self, event: OptionList.OptionSelected):
@@ -96,13 +98,12 @@ class Avocet(App):
         option_list.add_options(options)
 
     @on(OptionList.OptionSelected, selector="#raindrop_option_list")
-    def select_raidrop(self, event: OptionList.OptionSelected):
+    async def select_raidrop(self, event: OptionList.OptionSelected):
         log("selected")
         raindrop_id = event.option.id
         raindrop = self.database_manager.get_raindrop_by_raindrop_id(raindrop_id)
-        client_browser = webbrowser.get()
-        client_browser.open(raindrop.link)
-        # webbrowser.open(raindrop.link)
+        markdown = self.query_one("Markdown")
+        await markdown.update(raindrop.summary)
 
 
 if __name__ == "__main__":
